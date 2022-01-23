7705e27ad79c28e099931ad3d7a4a50654d70fa4
LukeRekt <lucasrsl12@gmail.com>
2022-01-23 20:30:15 -0300

    Base de rotas das paginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Company from './components/pages/Company';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/pages/layout/Container';
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+        <div>
+      <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+        
+      
+      <Routes>
+        <Container>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/company" element={<Company/>}></Route>
+      <Route exact path="/contact" element={<Contact/>}></Route>
+      <Route exact path="/newproject" element={<NewProject/>}></Route>
+      </Container>
+      </Routes>
+      <p>Footer</p>
+      
+  </Router>
+  
   );
 }
 
