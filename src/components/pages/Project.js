0e4155dca3efe453de9e18dc3be84e4225433cd4
LukeRekt <lucasrsl12@gmail.com>
2022-01-29 10:41:33 -0300

    area de servicos

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project(){
 
 const [project, setProject] = useState([]);
 const [showProjectForm, setShowProjectForm] = useState(false);
+const [showServiceForm, setShowServiceForm] = useState(false);
 const [message, setMessage] = useState();
 const [type, setType] = useState();
 
@@ -35,6 +36,7 @@ useEffect(() => {
 }, [id])
 
 function editPost(project){
+    setMessage('');
     // budget validation
 
     if(project.budget < project.cost){
@@ -62,38 +64,59 @@ function editPost(project){
 function toggleProjectForm(){
     setShowProjectForm(!showProjectForm);
 }
+function toggleServiceForm(){
+    setShowProjectForm(!showProjectForm);
+}
+
 
 
 
  
 return (
     <>
-    {project.name ?(
-        <div className={styles.project_details}>
-            <Container customClass="column">
-                {message && <Message type={type} msg={message}/>}
-                <div className={styles.project_details_container}>
-                    <h1>Projeto: {project.name}</h1>
-                    <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar projeto' : 'Fechar'}</button>
-               {!showProjectForm ? (
-                   <div className={styles.project_info}>
-                       <p>
-                           <span>Categoria:</span> {project.category.name}
-                           </p>
-                           <p>
-                               <span>Total de Orçamento:</span> {project.budget}
-                           </p>
-                           <p>
-                               <span>Total Utilizado:</span> R${project.cost}
-                           </p>
-                   </div>
-               ) : (
-                   <div className={styles.project_info}>
-                       <ProjectForm handleSubmit={editPost} btnText="Concluir edição" projectData={project}/>
-                   </div>
-               )}
+    {project.name ? (
+      <div className={styles.project_details}>
+        <Container customClass="column">
+          {message && <Message type={type} msg={message} />}
+          <div className={styles.details_container}>
+            <h1>Projeto: {project.name}</h1>
+            <button className={styles.btn} onClick={toggleProjectForm}>
+              {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+            </button>
+            {!showProjectForm ? (
+              <div className={styles.form}>
+                <p>
+                  <span>Categoria:</span> {project.category.name}
+                </p>
+                <p>
+                  <span>Total do orçamento:</span> R${project.budget}
+                </p>
+                <p>
+                  <span>Total utilizado:</span> R${project.cost}
+                </p>
+              </div>
+            ) : (
+              <div className={styles.form}>
+                <ProjectForm
+                  handleSubmit={editPost}
+                  btnText="Concluir Edição"
+                  projectData={project}
+                />
+              </div>
+            )}
                
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço:</h2>
+                    <button className={styles.btn} onClick={!showServiceForm}>{!showProjectForm ? 'Adicionar serviço' : 'Fechar'}</button>
+                    <div className={styles.form}>
+                        {showServiceForm && <div>formulário do serviço</div> }
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start">
+                <p>Itens de Servicos</p>
+                </Container>
             </Container>
         </div>
     ) : (
